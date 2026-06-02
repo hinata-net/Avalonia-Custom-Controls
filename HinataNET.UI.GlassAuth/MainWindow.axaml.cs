312d8ab86c7af60ea3f5a50fe38ee040d59a57f0
Buyko Dmitriy <dmitrijbujko9@gmail.com>
2026-06-02 18:01:53 +0300

    Add project files.

---
+++ HinataNET.UI.GlassAuth/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HinataNET.UI.GlassAuth
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
